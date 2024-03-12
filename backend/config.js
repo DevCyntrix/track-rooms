444eb79979fee7de52b2fa2fa6375fa8d9a3ecd1
Uni_laptop_ninjahorn <danielgellert09@gmail.com>
2024-03-12 20:33:57 +0100

    changes to api url

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,5 @@
 export const PORT = 5555;
 
 export const mongoDBURL = 'mongodb+srv://danielgellert09:oIJhWTvxZf3j0bkb@clustertrackrooms.6fyx3ns.mongodb.net/?retryWrites=true&w=majority&appName=ClusterTrackRooms'
+
+export const API_URL = 'http://localhost:5555';

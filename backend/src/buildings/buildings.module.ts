4f5a9c75147590083a6e1f3d9507ad5e3054bf47
Lucas Heinschke <Lucas@Heinschke.com>
2024-11-19 15:25:54 +0100

    implement building endpoint

--- backend/src/buildings/buildings.module.ts
+++ backend/src/buildings/buildings.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { BuildingsService } from './buildings.service';
 import { BuildingsController } from './buildings.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Room } from './entities/room.entity';
+import { Room } from 'src/rooms/entities/room.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Room])],
   controllers: [BuildingsController],
   providers: [BuildingsService],
 })
-export class RoomsModule {}
+export class BuildingsModule {}

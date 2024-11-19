69efc24dd9c558f30a43b134b47ad9c6209ad5a7
Ricardo Borutta <ricardo.borutta@hotmail.com>
2024-11-19 16:58:42 +0100

    Delete the old backend

--- backend_old/config.js
+++
@@ -1,4 +0,0 @@
-export const mongoDBURL = 'mongodb+srv://danielgellert09:oIJhWTvxZf3j0bkb@clustertrackrooms.6fyx3ns.mongodb.net/?retryWrites=true&w=majority&appName=ClusterTrackRooms'
-
-export const PORT = process.env.PORT || 5555;
-export const API_URL = 'http://trackrooms.azurewebsites.net';

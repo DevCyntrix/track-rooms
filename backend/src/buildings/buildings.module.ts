22b353d2c8ae9f99bbfa90e90303f4a4821e19f3
Ricardo Borutta <ricardo.borutta@hotmail.com>
2024-11-19 15:02:00 +0100

    Add buildings service

---
+++ backend/src/buildings/buildings.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { BuildingsService } from './buildings.service';
+import { BuildingsController } from './buildings.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Room } from './entities/room.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Room])],
+  controllers: [BuildingsController],
+  providers: [BuildingsService],
+})
+export class RoomsModule {}
